1e730e09922ed60f53c4b5dd2dfb73f850286800
Cedric <cedricmarcelino1105@gmail.com>
2022-02-17 18:08:01 +0800

    Added autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,24 @@
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor){
+    const originalMethod = descriptor.value
+    const newDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFunction = originalMethod.bind(this)
+            return boundFunction
+        }
+    }
+    return newDescriptor
+}
+
+
 class ProjectInput {
     templateElement: HTMLTemplateElement
     hostElement: HTMLDivElement
     element: HTMLFormElement
+    titleInputElement: HTMLInputElement
+    descriptionInputElement: HTMLInputElement
+    peopleInputElement: HTMLInputElement
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement
@@ -9,9 +26,28 @@ class ProjectInput {
 
         const importedNode = document.importNode(this.templateElement.content, true)
         this.element = importedNode.firstElementChild as HTMLFormElement
-        this.attach()
+        this.element.id = 'user-input'
 
+        this.titleInputElement = this.element.querySelector('#title')! 
+        this.descriptionInputElement = this.element.querySelector('#description')! 
+        this.peopleInputElement = this.element.querySelector('#people')! 
+
+
+        this.configure()
+        this.attach()
     }
+
+        @Autobind
+        private submitForm (event: Event) {
+            event.preventDefault()
+            console.log(this.titleInputElement.value)
+        }
+
+        private configure() {
+            this.element.addEventListener('submit', this.submitForm)
+        }
+
+
         private attach() {
             this.hostElement.insertAdjacentElement('afterbegin', this.element)
         }
