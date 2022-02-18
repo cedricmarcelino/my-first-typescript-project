f52ae4a4a811bfb598534efdcd81d195c5cbd6d4
Cedric <cedricmarcelino1105@gmail.com>
2022-02-18 17:39:23 +0800

    Added project list template to the DOM.

--- src/app.ts
+++ src/app.ts
@@ -47,7 +47,37 @@ function Autobind(_: any, _2: string, descriptor: PropertyDescriptor){
     return newDescriptor
 }
 
-//Class
+//Class for project list
+class ProjectList {
+    templateElement: HTMLTemplateElement
+    hostElement: HTMLDivElement
+    element: HTMLElement
+
+    constructor(private type: 'active' | 'finished'){
+        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement
+        this.hostElement = document.getElementById('app')! as HTMLDivElement
+
+        const importedNode = document.importNode(this.templateElement.content, true)
+        this.element = importedNode.firstElementChild as HTMLElement
+        this.element.id = `${this.type}-projects`
+
+        this.attach()
+        this.renderContent()
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element)
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`
+        this.element.querySelector('ul')!.id = listId
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS'
+    }
+
+}
+
+//Class for project form
 class ProjectInput {
     templateElement: HTMLTemplateElement
     hostElement: HTMLDivElement
@@ -139,3 +169,5 @@ class ProjectInput {
 }
 
 const projectInput = new ProjectInput()
+const activeProjectList = new ProjectList('active')
+const finishedProjectList = new ProjectList('finished')
